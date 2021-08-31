a0131e6960bf1f3985ec58d00d3b08aa64c38d1e
Michael B. Klein <mbklein@gmail.com>
2021-08-31 15:03:51 -0500

    Fix all eslint/yarn errors and warnings

--- tests/processor.test.js
+++ tests/processor.test.js
@@ -1,27 +1,30 @@
-'use strict';
+"use strict";
 
-const assert = require('assert');
-const iiif = require('../index');
+const assert = require("assert");
+const iiif = require("../index");
 
-var subject;
+let subject;
 
-describe('IIIF Processor', () => {
+describe("IIIF Processor", () => {
   beforeEach(() => {
-    subject = new iiif.Processor('https://example.org/iiif/2/ab/cd/ef/gh/i/10,20,30,40/pct:50/45/default.png', id => id);
+    subject = new iiif.Processor(
+      "https://example.org/iiif/2/ab/cd/ef/gh/i/10,20,30,40/pct:50/45/default.png",
+      (id) => id
+    );
   });
 
-  it('Parse URL', () => {
-    assert.strictEqual(subject.rotation, '45');
-    assert.strictEqual(subject.size, 'pct:50');
-    assert.strictEqual(subject.region, '10,20,30,40');
-    assert.strictEqual(subject.quality, 'default');
-    assert.strictEqual(subject.format, 'png');
+  it("Parse URL", () => {
+    assert.strictEqual(subject.rotation, "45");
+    assert.strictEqual(subject.size, "pct:50");
+    assert.strictEqual(subject.region, "10,20,30,40");
+    assert.strictEqual(subject.quality, "default");
+    assert.strictEqual(subject.format, "png");
   });
 
-  it('Create pipeline', () => {
-    var dims = { width: 1024, height: 768 };
-    var pipe = subject.pipeline(dims);
-    var opts = pipe.options;
+  it("Create pipeline", () => {
+    const dims = { width: 1024, height: 768 };
+    const pipe = subject.pipeline(dims);
+    const opts = pipe.options;
 
     assert.strictEqual(opts.width, 15);
     assert.strictEqual(opts.height, -1);
@@ -29,35 +32,44 @@ describe('IIIF Processor', () => {
     assert.strictEqual(opts.topOffsetPre, 20);
     assert.strictEqual(opts.widthPre, 30);
     assert.strictEqual(opts.heightPre, 40);
-    assert.strictEqual(opts.formatOut, 'png');
-    assert.strictEqual(opts.canvas, 'crop');
+    assert.strictEqual(opts.formatOut, "png");
+    assert.strictEqual(opts.canvas, "crop");
     assert.strictEqual(opts.withMetadata, false);
   });
 });
 
-describe('Include metadata', () => {
+describe("Include metadata", () => {
   beforeEach(() => {
-    subject = new iiif.Processor('https://example.org/iiif/2/ab/cd/ef/gh/i/10,20,30,40/pct:50/45/default.jpg', id => id, null, null, true);
+    subject = new iiif.Processor(
+      "https://example.org/iiif/2/ab/cd/ef/gh/i/10,20,30,40/pct:50/45/default.jpg",
+      (id) => id,
+      null,
+      null,
+      true
+    );
   });
 
-  it('Includes preexisting metadata', () => {
-    var dims = { width: 1024, height: 768 };
-    var pipe = subject.pipeline(dims);
-    var opts = pipe.options;
+  it("Includes preexisting metadata", () => {
+    const dims = { width: 1024, height: 768 };
+    const pipe = subject.pipeline(dims);
+    const opts = pipe.options;
 
     assert.strictEqual(opts.withMetadata, true);
   });
 });
 
-describe('TIFF Download', () => {
+describe("TIFF Download", () => {
   beforeEach(() => {
-    subject = new iiif.Processor('https://example.org/iiif/2/ab/cd/ef/gh/i/10,20,30,40/pct:50/45/default.tif', id => id);
+    subject = new iiif.Processor(
+      "https://example.org/iiif/2/ab/cd/ef/gh/i/10,20,30,40/pct:50/45/default.tif",
+      (id) => id
+    );
   });
 
-  it('Create pipeline', () => {
-    var dims = { width: 1024, height: 768 };
-    var pipe = subject.pipeline(dims);
-    var opts = pipe.options;
+  it("Create pipeline", () => {
+    const dims = { width: 1024, height: 768 };
+    const pipe = subject.pipeline(dims);
+    const opts = pipe.options;
 
     assert.strictEqual(opts.width, 15);
     assert.strictEqual(opts.height, -1);
@@ -65,7 +77,7 @@ describe('TIFF Download', () => {
     assert.strictEqual(opts.topOffsetPre, 20);
     assert.strictEqual(opts.widthPre, 30);
     assert.strictEqual(opts.heightPre, 40);
-    assert.strictEqual(opts.formatOut, 'tiff');
-    assert.strictEqual(opts.canvas, 'crop');
+    assert.strictEqual(opts.formatOut, "tiff");
+    assert.strictEqual(opts.canvas, "crop");
   });
 });
