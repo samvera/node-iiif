d764e9ad66115e72709911728cb4f2f307b7ed13
Michael B. Klein <mbklein@gmail.com>
2021-10-26 18:59:11 -0500

    Add close to 99%+ test coverage

---
+++ tests/integration.test.js
@@ -0,0 +1,109 @@
+'use strict';
+
+const assert = require('assert');
+const fs = require('fs');
+const iiif = require('../index');
+const probe = require('probe-image-size');
+const { qualities, formats, regions, sizes, rotations } = require('./fixtures/iiif-values');
+
+const base = 'https://example.org/iiif/2/ab/cd/ef/gh/i';
+const streamResolver = () => fs.createReadStream('./tests/fixtures/samvera.tif');
+let subject;
+let consoleWarnMock;
+
+describe('info.json', () => {
+  beforeEach(() => {
+    subject = new iiif.Processor(`${base}/info.json`, streamResolver);
+  });
+
+  it('produces a valid info.json', async () => {
+    const result = await subject.execute();
+    const info = JSON.parse(result.body);
+    assert.strictEqual(info.width, 621);
+    assert.strictEqual(info.height, 327);
+  });
+});
+
+describe('quality', () => {
+  qualities.forEach((value) => {
+    it(`should produce an image with quality ${value}`, async () => {
+      subject = new iiif.Processor(`${base}/full/full/0/${value}.png`, streamResolver);
+      const result = await subject.execute();
+      assert.strictEqual(result.contentType, 'image/png');
+    });
+  });
+});
+
+describe('format', () => {
+  formats.forEach((value) => {
+    it(`should produce an image with format ${value}`, async () => {
+      subject = new iiif.Processor(`${base}/full/full/0/default.${value}`, streamResolver);
+      const result = await subject.execute();
+      assert.match(result.contentType, /^image\//);
+    });
+  });
+});
+
+describe('region', () => {
+  regions.forEach((value) => {
+    it(`should produce an image with region ${value}`, async () => {
+      subject = new iiif.Processor(`${base}/${value}/full/0/default.png`, streamResolver);
+      const result = await subject.execute();
+      assert.strictEqual(result.contentType, 'image/png');
+    });
+  });
+
+  it('should require valid region size', async () => {
+    subject = new iiif.Processor(`${base}/0,0,0,0/full/0/default.png`, streamResolver);
+    assert.rejects(() => subject.execute(), iiif.IIIFError);
+  });
+});
+
+describe('size', () => {
+  sizes.forEach((value) => {
+    it(`should produce an image with size ${value}`, async () => {
+      subject = new iiif.Processor(`${base}/full/${value}/0/default.png`, streamResolver);
+      const result = await subject.execute();
+      assert.strictEqual(result.contentType, 'image/png');
+    });
+  });
+
+  it('should require valid size', async () => {
+    subject = new iiif.Processor(`${base}/full/pct:0/0/default.png`, streamResolver);
+    assert.rejects(() => subject.execute(), iiif.IIIFError);
+  });
+});
+
+describe('rotation', () => {
+  rotations.forEach((value) => {
+    it(`should produce an image with rotation ${value}`, async () => {
+      subject = new iiif.Processor(`${base}/full/full/${value}/default.png`, streamResolver);
+      const result = await subject.execute();
+      assert.strictEqual(result.contentType, 'image/png');
+    });
+  });
+});
+
+describe('IIIF transformation', () => {
+  beforeEach(() => {
+    consoleWarnMock = jest.spyOn(global.console, 'warn').mockImplementation();
+    subject = new iiif.Processor(
+      `${base}/10,20,30,40/pct:50/45/default.png`,
+      streamResolver,
+      { dimensionFunction: () => null }
+    );
+  });
+
+  afterEach(() => {
+    consoleWarnMock.mockRestore();
+  });
+
+  it('transforms the image', async () => {
+    const result = await subject.execute();
+    const size = await probe.sync(result.body);
+
+    assert.strictEqual(size.width, 25);
+    assert.strictEqual(size.height, 25);
+    assert.strictEqual(size.mime, 'image/png');
+  });
+});
--- tests/processor.test.js
+++ tests/processor.test.js
@@ -1,28 +1,26 @@
-"use strict";
+'use strict';
 
-const assert = require("assert");
-const iiif = require("../index");
+const assert = require('assert');
+const iiif = require('../index');
 
 let subject;
+const base = 'https://example.org/iiif/2/ab/cd/ef/gh/i';
+const dims = { width: 1024, height: 768 };
 
-describe("IIIF Processor", () => {
+describe('IIIF Processor', () => {
   beforeEach(() => {
-    subject = new iiif.Processor(
-      "https://example.org/iiif/2/ab/cd/ef/gh/i/10,20,30,40/pct:50/45/default.png",
-      (id) => id
-    );
+    subject = new iiif.Processor(`${base}/10,20,30,40/pct:50/45/default.png`, (id) => id);
   });
 
-  it("Parse URL", () => {
-    assert.strictEqual(subject.rotation, "45");
-    assert.strictEqual(subject.size, "pct:50");
-    assert.strictEqual(subject.region, "10,20,30,40");
-    assert.strictEqual(subject.quality, "default");
-    assert.strictEqual(subject.format, "png");
+  it('Parse URL', () => {
+    assert.strictEqual(subject.rotation, '45');
+    assert.strictEqual(subject.size, 'pct:50');
+    assert.strictEqual(subject.region, '10,20,30,40');
+    assert.strictEqual(subject.quality, 'default');
+    assert.strictEqual(subject.format, 'png');
   });
 
-  it("Create pipeline", () => {
-    const dims = { width: 1024, height: 768 };
+  it('Create pipeline', () => {
     const pipe = subject.pipeline(dims);
     const opts = pipe.options;
 
@@ -32,25 +30,22 @@ describe("IIIF Processor", () => {
     assert.strictEqual(opts.topOffsetPre, 20);
     assert.strictEqual(opts.widthPre, 30);
     assert.strictEqual(opts.heightPre, 40);
-    assert.strictEqual(opts.formatOut, "png");
-    assert.strictEqual(opts.canvas, "crop");
+    assert.strictEqual(opts.formatOut, 'png');
+    assert.strictEqual(opts.canvas, 'crop');
     assert.strictEqual(opts.withMetadata, false);
   });
 });
 
-describe("Include metadata", () => {
+describe('Include metadata', () => {
   beforeEach(() => {
     subject = new iiif.Processor(
-      "https://example.org/iiif/2/ab/cd/ef/gh/i/10,20,30,40/pct:50/45/default.jpg",
+      `${base}/10,20,30,40/pct:50/45/default.jpg`,
       (id) => id,
-      null,
-      null,
-      true
+      { includeMetadata: true }
     );
   });
 
-  it("Includes preexisting metadata", () => {
-    const dims = { width: 1024, height: 768 };
+  it('Includes preexisting metadata', () => {
     const pipe = subject.pipeline(dims);
     const opts = pipe.options;
 
@@ -58,16 +53,12 @@ describe("Include metadata", () => {
   });
 });
 
-describe("TIFF Download", () => {
+describe('TIFF Download', () => {
   beforeEach(() => {
-    subject = new iiif.Processor(
-      "https://example.org/iiif/2/ab/cd/ef/gh/i/10,20,30,40/pct:50/45/default.tif",
-      (id) => id
-    );
+    subject = new iiif.Processor(`${base}/10,20,30,40/pct:50/45/default.tif`, (id) => id);
   });
 
-  it("Create pipeline", () => {
-    const dims = { width: 1024, height: 768 };
+  it('Output TIFF format', () => {
     const pipe = subject.pipeline(dims);
     const opts = pipe.options;
 
@@ -77,7 +68,93 @@ describe("TIFF Download", () => {
     assert.strictEqual(opts.topOffsetPre, 20);
     assert.strictEqual(opts.widthPre, 30);
     assert.strictEqual(opts.heightPre, 40);
-    assert.strictEqual(opts.formatOut, "tiff");
-    assert.strictEqual(opts.canvas, "crop");
+    assert.strictEqual(opts.formatOut, 'tiff');
+    assert.strictEqual(opts.canvas, 'crop');
+  });
+});
+
+describe('Density', () => {
+  beforeEach(() => {
+    subject = (ext) => {
+      return new iiif.Processor(
+        `https://example.org/iiif/2/ab/cd/ef/gh/i/10,20,30,40/pct:50/45/default.${ext}`,
+        (id) => id,
+        { density: 600 }
+      );
+    };
+  });
+
+  it('Adds density to TIFF', () => {
+    const processor = subject('tif');
+    const pipe = processor.pipeline(dims);
+    const opts = pipe.options;
+
+    assert.strictEqual(opts.tiffXres, 600 / 25.4);
+    assert.strictEqual(opts.tiffYres, 600 / 25.4);
+  });
+
+  it('Adds density to JPEG', () => {
+    const processor = subject('jpg');
+    const pipe = processor.pipeline(dims);
+    const opts = pipe.options;
+
+    assert.strictEqual(opts.withMetadataDensity, 600);
+  });
+
+  it('Adds density to PNG', () => {
+    const processor = subject('png');
+    const pipe = processor.pipeline(dims);
+    const opts = pipe.options;
+
+    assert.strictEqual(opts.withMetadataDensity, 600);
+  });
+});
+
+describe('constructor', () => {
+  it('must parse the object-based constructor', () => {
+    subject = new iiif.Processor(
+      `${base}/10,20,30,40/pct:50/45/default.tif`,
+      () => 'streamResolver', 
+      { dimensionFunction: () => 'dimensionFunction', maxWidth: 'maxWidth', includeMetadata: true, density: 600 }
+    );
+
+    assert.strictEqual(subject.streamResolver(), 'streamResolver');
+    assert.strictEqual(subject.dimensionFunction(), 'dimensionFunction');
+    assert.strictEqual(subject.maxWidth, 'maxWidth');
+    assert.strictEqual(subject.includeMetadata, true);
+    assert.strictEqual(subject.density, 600);
+  });
+
+  it('must parse the multi-argument constructor', () => {
+    const spy = jest.spyOn(global.console, 'warn').mockImplementation();
+    subject = new iiif.Processor(
+      `${base}/10,20,30,40/pct:50/45/default.tif`,
+      () => 'streamResolver',
+      () => 'dimensionFunction',
+      'maxWidth',
+      true
+    );
+    expect(spy).toHaveBeenCalledWith(expect.stringMatching(/deprecated/));
+    spy.mockRestore();
+
+    assert.strictEqual(subject.streamResolver(), 'streamResolver');
+    assert.strictEqual(subject.dimensionFunction(), 'dimensionFunction');
+    assert.strictEqual(subject.maxWidth, 'maxWidth');
+    assert.strictEqual(subject.includeMetadata, true);
+    assert.strictEqual(subject.density, null);
+  });
+});
+
+describe('constructor errors', () => {
+  it('requires a streamResolver', () => {
+    assert.throws(() => {
+      return new iiif.Processor(`${base}/10,20,30,40/pct:50/45/default.tif`, {});
+    }, iiif.IIIFError);
+  });
+
+  it('requires a valid URL', () => {
+    assert.throws(() => {
+      return new iiif.Processor(`${base}/10,20,30,40/pct:50/45/default.blargh`, (id) => id);
+    }, iiif.IIIFError);
   });
 });
