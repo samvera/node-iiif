92e77814047ad892a3647e7040a8199ea211c6b0
Michael B. Klein <mbklein@gmail.com>
2025-01-13 10:45:21 -0600

    Add index page to tiny-iiif example

--- examples/tiny-iiif/index.js
+++ examples/tiny-iiif/index.js
@@ -2,6 +2,7 @@
 
 import { App } from '@tinyhttp/app';
 import { logger } from '@tinyhttp/logger';
+import serveStatic from 'serve-static';
 import { port, iiifImagePath } from './config.js';
 import IIIFRouter from './iiif.js';
 
@@ -9,7 +10,9 @@ const app = new App({ settings: { networkExtensions: true } });
 
 app
   .use(logger())
-  .get('/', (_req, res) => res.redirect('/iiif/2'))
   .use('/iiif/2', IIIFRouter(2))
   .use('/iiif/3', IIIFRouter(3))
+  .use('/openseadragon', serveStatic('./node_modules/openseadragon/build/openseadragon'))
+  .use('/images', serveStatic('./node_modules/openseadragon/build/openseadragon/images'))
+  .use('/', serveStatic('./public'))
   .listen(port, () => console.log(`Serving IIIF images from ${iiifImagePath} on http://localhost:${port}`));
